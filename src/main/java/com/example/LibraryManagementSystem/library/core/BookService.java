a2660f0adc8b9c79141a23c776cfe21a7307ab2d
Zero-definition <saathvikchoudhary@gmail.com>
2024-04-10 10:52:47 +0530

    Notes

--- src/main/java/com/example/LibraryManagementSystem/library/core/BookService.java
+++ src/main/java/com/example/LibraryManagementSystem/library/core/BookService.java
@@ -1,12 +1,10 @@
 package com.example.LibraryManagementSystem.library.core;
 
 import com.example.LibraryManagementSystem.library.Data.*;
-import com.example.LibraryManagementSystem.library.domain.Book;
 import com.example.LibraryManagementSystem.library.persistence.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.Objects;
 
 @Service
@@ -14,6 +12,68 @@ public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+
+
+    /**
+     *      in findall we shpuld add RequestPage(0,100)
+     */
+
+    // response entity represents a http response
+
+    /**
+     *      return response.ok()? response.:ResponseEntity.ok(response):ResponseEntity.badRequest().body();
+     *      use this to check and return an error or the correct required response
+     *
+     *      ResponseEntity defines a http response which is given by the API
+     *
+     *      Whenever the caller is adding details into the repository we also need to send a
+     *      response containing all the details entered by the user
+     *
+     *
+     *      RequestEntity.ok()
+     *      RequestEntity.status(HttpStatus.CREATED).body(response)
+     *      ResponseEntity.badRequest().body(response)    // bad request also sends a proper response to the user
+     *      Check out all the ResponseEntity. functions that are available
+     *
+     *
+     *      What is the difference betwen GetMapping and PostMApping
+     *
+     *
+     *
+     *      Return statements based on the request type
+     *
+     * Request types:-
+     *      -> get
+     *          ResponseEntity.ok()
+     *          ResponseEntity.notfound
+     *
+     *      -> save
+     *          ResponseEntity.ok
+     *          ResponseEntity.badrequest
+     *          ResponseEntity.notfound
+     *          ResponseEntity.saved
+     *
+     *      -> async
+     *          ResponseEntity.accepted
+     *
+     *
+     ****************************************************************************************************************************************************************
+     *      Asynchronous request processing
+     *
+     *
+     *      Thread.sleep(); //this can be used to delay the thread before returning the result from the call
+     *
+     *
+     *      when we are using multiple sleeps then try to use parallel calls so that the number of calls won't compound the required time
+     *      ^
+     *      | the above line is achieved by using @Async annotation in spring boot
+     *                          return services.parallelstream() instead of services.stream()
+     *
+     *
+     *      ResponseEntity.accepted().build() is used whenever we are working with @Async annotation
+     *
+     ****************************************************************************************************************************************************************
+     */
 
 
     public GetBookByTitleResponse getBookByTitle(final GetBookByTitleRequest request)
